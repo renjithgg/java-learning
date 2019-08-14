681f69b1374f59bd0973e4a339dfb8d6881d798e
renjithgg <renjithgg@gmail.com>
2019-08-14 11:59:38 +0530

    Core Java learn concepts through program : 14-Aug-2019

--- JavaGenerics/JavaGenerics.java
+++ JavaGenerics/JavaGenerics.java
@@ -1,6 +1,6 @@
 class Base<T1, T2> {
-	T1 obj1;
-	T2 obj2;
+	private T1 obj1;
+	private T2 obj2;
 	Base(T1 obj1Val, T2 obj2Val) {
 		this.obj1 = obj1Val;
 		this.obj2 = obj2Val;
@@ -17,22 +17,43 @@ class Base<T1, T2> {
 	public<TV> TV displayValue(TV value) {
 		return value;
 	}
+	
+	public void setFirstObject(T1 value) {
+		this.obj1 = value;
+	}
+	
+	public T1 getFirstObject() {
+		return obj1;
+	}
+	
+	public void setSecondObject(T2 value) {
+		this.obj2 = value;
+	}
+	
+	public T2 getSecondObject() {
+		return obj2;
+	}
 }
 
 class JavaGenerics {
 	public static void main(String args[]) {
 		Base<String, Integer> b1 = new Base<String, Integer>("Renjith", 641);
-		System.out.println("Name = "+ b1.obj1);
-		System.out.println("Id = "+ b1.obj2);
+		System.out.println("Name = "+ b1.getFirstObject());
+		System.out.println("Id = "+ b1.getSecondObject());
 		
 		Base<Double, Character> b2 = new Base<Double, Character>(12.5, 'R');
-		System.out.println("Pay money = "+ b2.obj1);
-		System.out.println("Pay char = "+ b2.obj2);
+		System.out.println("Pay money = "+ b2.getFirstObject());
+		System.out.println("Pay char = "+ b2.getSecondObject());
 		
 		b1.display("Hello Renjith");
 		b1.display(10);
 		
 		System.out.println(b1.displayValue(200));
+		
+		
+		b2.setSecondObject('R');
+		
+		System.out.println(b2.getSecondObject());
 		
 	}
 }
